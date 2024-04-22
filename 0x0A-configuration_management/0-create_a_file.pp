b91605af622e72811a221b67e2ce99355aba3700
root <magma.mg@gmail.com>
2024-04-22 03:09:36 -0700

    lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Create a File
-File { '/tmp/school'
-permission => '0744'
-owner => 'www-data'
-group => 'www-data'
-contains => 'I love Puppet'
+file { '/tmp/school'
+  ensure => file,
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
